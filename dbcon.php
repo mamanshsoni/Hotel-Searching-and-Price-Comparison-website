b175cf7b57ad7c26d06f0cc43977461483e5e91a
mamanshsoni <mamanshsoni@gmail.com>
2021-05-29 22:37:17 +0530

    Initial commit

---
+++ dbcon.php
@@ -0,0 +1,16 @@
+<?php
+
+// $con = mysqli_connect('localhost','vtcbtpbw_vtcbtpbw','dkmo(d+Hp~5G))','vtcbtpbw_fsdata');
+
+
+// if($con)
+// {
+// 	//echo "<h1> we are now connected to database </h1>";
+// }
+// else
+// {
+
+// 	echo "<h1> we are not connected to database </h1>";
+// }
+$con=mysqli_connect('localhost','vtcbtpbw_vtcbtpbw','dkmo(d+Hp~5G))','vtcbtpbw_fsdata');if($con) {} else {echo"<h1> we are not connected to database </h1>";}
+?>
